4b6a869972c2729410242801ce1fa8aba5eac38d
IshakBenfredj <ishakbanfradje@gmail.com>
2025-02-13 10:55:59 +0100

    changes

--- ms-patient/src/main/java/com/esi/mspatient/Proxy/LBConfiguration.java
+++ ms-patient/src/main/java/com/esi/mspatient/Proxy/LBConfiguration.java
@@ -1,12 +1,12 @@
 package com.esi.mspatient.Proxy;
 
-import org.hibernate.cfg.Environment;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 public class LBConfiguration {
     @Bean
